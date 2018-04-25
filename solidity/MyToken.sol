a6c96e2ac2cfd5954df2d5e896e0c8940bb7f2c9
gfesquet <gfesquet@ingimax.com>
2018-04-25 03:28:44 +0200

    token tutorial WARN to avoid 'Error: invalid address' set eth.defaultAccount or use {from: ...} as the last param of the function

--- solidity/MyToken.sol
+++ solidity/MyToken.sol
@@ -12,7 +12,7 @@ contract MyToken {
     }
 
     /* Send coins */
-    function transfer(address _to, uint256 _value) public {
+    function transfer(address _to, uint256 _value) public payable {
         require(balanceOf[msg.sender] >= _value);           // Check if the sender has enough
         require(balanceOf[_to] + _value >= balanceOf[_to]); // Check for overflows
         balanceOf[msg.sender] -= _value;                    // Subtract from the sender
